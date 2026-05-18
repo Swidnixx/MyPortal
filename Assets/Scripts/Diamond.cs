c948284bdb44bea9c623b2aece89de41b7b8f5d1
Swidnixx <swidnixx@gmail.com>
2026-05-18 20:13:16 +0200

    WinSpot

--- Assets/Scripts/Diamond.cs
+++ Assets/Scripts/Diamond.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class Diamond : Pickup
 {
+    public AudioClip clip;
     protected override void Pick()
     {
         base.Pick(); //Destroy
 
+        SoundManager.Instance.PlaySFX(clip);
         GameManager.Instance.points++;
     }
 }
